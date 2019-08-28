8983783f43b3848e121a6887081be38cd3e1af2f
Gabor Benko <benkogabor@msn.com>
2019-08-28 17:28:58 +0200

    testlab

--- semantic.py
+++ semantic.py
@@ -1,8 +1,9 @@
 from dataset import load_image
 import torch
-from generate_masks import get_model
+from generate_masks import get_model, img_transform
 from functions import img_to_tensor
 import numpy as np
+import cv2
 
 arrt = np.random.randn(500, 500)
 tensor = img_to_tensor(arrt)
@@ -10,3 +11,8 @@ print(tensor)
 
 model_path = 'pretrained/linknet_binary_20/model_0.pt'
 model = get_model(model_path, model_type='LinkNet34', problem_type='binary')
+
+img =  load_image('pretrained/test.jpg')
+img_tensor = img_to_tensor(img)
+input_img = torch.unsqueeze(img_to_tensor(img_transform(p=1)(image=img)['image']), dim=0)
+mask = model(input_img)
