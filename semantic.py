f47a7ac5374046206a536a4ea90e1d80f27f44df
Gabor Benko <benkogabor@msn.com>
2019-09-01 11:04:24 +0200

    tested widescreen image

--- semantic.py
+++ semantic.py
@@ -12,7 +12,9 @@ print(tensor)
 model_path = 'pretrained/linknet_binary_20/model_0.pt'
 model = get_model(model_path, model_type='LinkNet34', problem_type='binary')
 
-img =  load_image('pretrained/test.jpg')
+img =  load_image('pretrained/test2.png')
 img_tensor = img_to_tensor(img)
 input_img = torch.unsqueeze(img_to_tensor(img_transform(p=1)(image=img)['image']), dim=0)
 mask = model(input_img)
+print("printing mask")
+print(mask)
