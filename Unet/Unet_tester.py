701443ab05d52f7d91d8e969374b86a58807ae28
Gábor Benkő <benkogabor@msn.com>
2019-10-04 22:16:09 +0200

    train test

--- Unet/Unet_tester.py
+++ Unet/Unet_tester.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from Unet import UNet
 from UnetDataset import ToolDataset
 
@@ -9,12 +10,26 @@ pixdeviator = 255
 dataset = ToolDataset('/home/bennyg/Development/datasets/instrument_dataset_1', mean, 
                       std, pixdeviator, input_size)
 dataloader = torch.utils.data.DataLoader(dataset, shuffle=False)
-
+number_epoch = 5
+learning_rate = 1e-1
 net = UNet(4)
-
-for image_src, image_mask in dataloader:
-    pred_mask = net(image_src)
-    print(pred_mask.shape)
-    break
-
-
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
+criterion = nn.BCEWithLogitsLoss()
+number_train = len(dataloader)
+for i in range(number_epoch):
+    epoch_loss = 0.0
+    loss_index = 0
+    net.train()
+    for image_src, image_mask in dataloader:
+        loss_index += 1
+        pred_mask = net(image_src)
+        prob_mask_flat = pred_mask.view(-1)
+        true_mask_flat = image_mask.view(-1)
+        loss = criterion(prob_mask_flat, true_mask_flat)
+        epoch_loss += loss.item()
+        print('{0}/{1} --- loss: {2:.6f}'.format(loss_index, number_train, loss.item()))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+          
+    print('Epoch: {} / {} Loss: {0:.4f}'.format(i + 1, number_epoch, epoch_loss / loss_index))
